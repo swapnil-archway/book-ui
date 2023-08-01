6d913cee82169a5f8e232537706e85975fe9a828
swapnil-archway <dgaurav497@gmail.com>
2023-08-01 15:23:01 +0530

    hot fix

--- pages/home.tsx
+++ pages/home.tsx
@@ -4,14 +4,13 @@ import { useRecoilState } from 'recoil';
 import { bookState } from '@/recoil/recoil';
 import { getBooks } from '@/api/api';
 import { Book, BookList, BookResponse, ListMeta } from '@/models/book';
-import { Loader } from '@/components/loader';
-import InfiniteScroll from 'react-infinite-scroll-component';
+import { usePullToRefresh } from '@/hooks/usePullToRefresh';
 
 const HomePage = () => {
   const [bookData, setBookData] = useRecoilState<BookList>(bookState);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
   const [errMessage, setErrMessage] = useState<string>('');
-  const pageSize = 10;
   const listRef = useRef<HTMLDivElement>(null);
 
   const fetchBooks = async (page: number): Promise<BookResponse> => {
@@ -31,16 +30,49 @@ const HomePage = () => {
     try {
       setIsLoading(true);
       const bookResponse = await fetchBooks(page);
+
       setBookData((prevData: BookList) => ({
         ...prevData,
         books: prevData ? prevData.books.concat(bookResponse.data) : bookResponse.data,
         currentPage: page,
-        total: bookResponse.total
+        total: bookResponse.total,
       }));
       setIsLoading(false);
+      setIsFetching(false);
     } catch (error) {
+      setIsFetching(false);
       console.error(error);
       setIsLoading(false);
+    }
+  };
+
+  const refreshBooksData = async (): Promise<void> => {
+    try {
+      const bookResponse: BookResponse = await fetchBooks(1);
+      setBookData({
+        books: bookResponse.data,
+        currentPage: 1,
+        limit: 10,
+        total: bookResponse.total,
+      });
+    } catch (error) {
+      console.error(error);
+      setBookData((prevData: BookList) => prevData);
+    }
+  };
+
+  const handleScroll = () => {
+    const container = listRef.current;
+    if (container) {
+      const scrollTop = container.scrollTop;
+      const scrollHeight = container.scrollHeight;
+      const clientHeight = container.clientHeight;
+      if (Math.round((scrollTop / (scrollHeight - clientHeight)) * 100) >= 80) {
+        if (!isLoading && !isFetching && bookData.books.length < bookData.total) {
+          setIsFetching(true);
+          loadBooksData((bookData?.currentPage ?? 1) + 1);
+        }
+      }
     }
   };
 
@@ -60,32 +92,20 @@ const HomePage = () => {
   }, [errMessage]);
 
   useEffect(() => {
-    // Attach the scroll event listener when the component mounts
     listRef.current?.addEventListener('scroll', handleScroll);
 
-    // Clean up the scroll event listener when the component unmounts
     return () => {
       listRef.current?.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [[isLoading]]);
 
-  const handleScroll = () => {
-    const container = listRef.current;
-    if (container) {
-      const scrollTop = container.scrollTop; // 5
-      const scrollHeight = container.scrollHeight; // 5000
-      const clientHeight = container.clientHeight; // 562
-      if (Math.round(scrollTop / (scrollHeight - clientHeight) * 100) >= 80) {
-        if (!isLoading && bookData.books.length < bookData.total) {
-          fetchBooks((bookData?.currentPage ?? 1) + 1);
-        }
-      }
-    }
-  };
+  const isRefreshing: boolean = usePullToRefresh(listRef, refreshBooksData);
+
   return (
     <div className="h-screen flex flex-col">
       <div className="m-4 text-xl text-center font-bold">Books</div>
       <div className="flex-grow overflow-y-auto" ref={listRef}>
+        {isRefreshing && <div className="m-4 text-lg">Refreshing...</div>}
         {bookData && bookData.books.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-1">
             {bookData &&
@@ -96,31 +116,6 @@ const HomePage = () => {
         ) : (
           <div className="text-center">No book found</div>
         )}
-
-        {/* <InfiniteScroll
-          dataLength={bookData?.books?.length || 0}
-          next={() => loadBooksData((bookData?.currentPage || 1) + 1)}
-          hasMore={(bookData?.books?.length ?? 0) < (bookData?.currentPage || 1) * pageSize}
-          loader={isLoading && <Loader />}
-          endMessage={<h4>No more books to load.</h4>}
-          refreshFunction={() => loadBooksData(1)}
-          pullDownToRefresh
-          pullDownToRefreshThreshold={200}
-          pullDownToRefreshContent={
-            <Loader />
-          }
-        >
-          {bookData && bookData.books.length > 0 ? (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-1">
-              {bookData &&
-                bookData.books?.map((book: Book) => {
-                  return <BookCard key={book.id} book={book} />;
-                })}
-            </div>
-          ) : (
-            <div className="text-center">No book found</div>
-          )}
-        </InfiniteScroll> */}
       </div>
 
       {errMessage && (
